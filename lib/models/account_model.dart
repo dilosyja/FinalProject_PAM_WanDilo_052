137e7c75a3e23a7d587930e7ba5ca085405c5fdf
Dilo Syuja <wandilo123@gmail.com>
2023-11-30 00:42:42 +0700

    commit

--- lib/models/account_model.dart
+++ lib/models/account_model.dart
@@ -1,5 +1,5 @@
 import 'package:hive/hive.dart';
-part 'account_model.g.dart'; // File generated code Hive
+part 'account_model.g.dart'; // File generated code Hivee
 
 @HiveType(typeId: 0)
 class AccountModel extends HiveObject {
